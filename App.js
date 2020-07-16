86b4eb4104a640bc3f41feb3095c12bb04e7df50
Shin Chae Yeon <rachaelshin@naver.com>
2020-07-16 22:27:42 +0900

    add todo

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './Header'
+import Body from './Body'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  state = {
+    todos: []
+  }
+  addTodo = (todo) => {
+    const newTodo = {
+      id: Date.now(),
+      text: todo,
+      completed: false,
+    }
+    this.setState(prevState => ({
+      todos: [
+        newTodo,
+        ...prevState.todos
+      ]
+    }));
+    console.log(this.state.todos);
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>나의 버킷리스트</Text>
+        <Header addTodo={this.addTodo}/>
+        <Body todos={this.state.todos}/>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column',
+    paddingTop: 50,
+    backgroundColor: "#EEE",
   },
+  title: {
+    fontWeight: "800",
+    fontSize: 30, 
+    marginLeft: 20,
+    marginBottom: 20,
+  }
 });
